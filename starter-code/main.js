62832e4492583fab2ec24739baeeeab99b0bfaab
Kelvin Tan Yeow Boon <Kelvin@Kelvins-MacBook-Air.local>
2016-09-16 18:37:40 +0800

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,9 @@
-console.log("JS file is connected to HTML! Woo!")
+console.log("JS file is connected to HTML! Woo!");
+
+let cardOne,cardTwo,cardThree,cardFour;
+
+cardOne = "queen";
+cardTwo = "queen";
+cardThree = "king";
+cardFour = "king";
+
